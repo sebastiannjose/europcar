a907a73310ea3d8ffb56b45665a287f6f3661379
Sebastian Njose <s3bastian06@gmail.com>
2016-09-12 08:03:43 +0300

    Minor change to participe email template

--- resources/views/emails/participe.blade.php
+++ resources/views/emails/participe.blade.php
@@ -1,14 +1,12 @@
-<h3>Hello User,</h3>
-<p>Thank you for joining the programme!</p>
-<p>If you want a copy of the Inscreva-se form's registrations forwarded to your email address in  excel format. Please use the link below;</p>
-<p>&nbsp;</p>
+<p>Thank you for looking at my entry. I have shared the Inscreva-se records in the excel attachment.</p>
+<p>Please follow the link below to my Github profile to review the source code.</p>
 <p>
-	<strong>
-		<a href="#">Receive Excel Updates</a>
-	</strong>
+        <strong>
+                <a href="https://github.com/sebastiannjose/europcar">Get source code</a>
+        </strong>
 </p>
 <p>&nbsp;</p>
 <p>Best,</p>
 <p>Njose</p>
 <p>&nbsp;</p>
-<p>&nbsp;</p>
+~                      
